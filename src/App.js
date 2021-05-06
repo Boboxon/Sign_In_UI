a797535816aab8ac0e2dfb9512d9d0a806c254b2
Rajabov Bobokhon <rboboxon6055@gmail.com>
2021-05-06 23:23:58 +0900

    adding file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Form from "./Component/Form";
 function App() {
   return (
     <div className="App">
+      <h2 style={{ textAlign: "center" }}>Register UI</h2>
       <Form />
     </div>
   );
